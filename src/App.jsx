0992b36c106c16a0401e90694d886c5cf234ecef
Om Preetham <ompreethambandi@gmail.com>
2024-03-26 22:29:58 -0500

    ready to deploy

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import ResultsSummary from './assets/components/ResultsSummary'
+
+const App = () => {
+  return (
+    <main>
+      <ResultsSummary />
+    </main>
+  )
+}
+export default App
---
+++ src/assets/components/ResultsSummary.jsx
@@ -0,0 +1,68 @@
+import data from '../../data/data.json'
+
+const ResultsSummary = () => {
+  return (
+    <>
+      <div className="wrapper">
+        <section>
+          <div className="layout-grid">
+            <article>
+              <div className="result-wrapper">
+                <div>
+                  <h4>Your Result</h4>
+                </div>
+                <div className="result__circle">
+                  <p>
+                    <span>76</span>
+                    <span>of 100</span>
+                  </p>
+                </div>
+                <div className="result__description">
+                  <p>
+                    <strong>Great</strong>
+                  </p>
+                  <p className="description">
+                    You scored higher than 65% of the people who have taken
+                    these tests.
+                  </p>
+                </div>
+              </div>
+            </article>
+            <article>
+              <div className="summary-wrapper">
+                <div>
+                  <h4>Summary</h4>
+                </div>
+                <div className="summary-list">
+                  {data.map((list, index) => {
+                    return (
+                      <div key={index} className={`${list.category}-list`}>
+                        <div>
+                          <img
+                            src={`${list.icon}`}
+                            alt={`${list.category} Icon`}
+                          />
+                          <p>{list.category}</p>
+                        </div>
+                        <div>
+                          <p>
+                            <strong>{list.score}</strong>
+                            <span> / 100</span>
+                          </p>
+                        </div>
+                      </div>
+                    )
+                  })}
+                </div>
+                <div>
+                  <button>Continue</button>
+                </div>
+              </div>
+            </article>
+          </div>
+        </section>
+      </div>
+    </>
+  )
+}
+export default ResultsSummary
